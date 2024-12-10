e5773d81b0dc98287e0410ed92bb9d7f7b8bee12
guyder0 <nik.volodchenkov@gmail.com>
2024-12-10 10:43:56 +0300

    th + del_tfrec
    написал теорию по слоям для генераторов и дискриминаторов + удалил неиспользуемые данные типа .tfrec

--- bot.py
+++ bot.py
@@ -1,19 +1,25 @@
 import requests, time, os
+import backend # тут загружается и будет использоваться обученная модель
 
-TOKEN = os.environ.get('SE-CW_TOKEN')
-API_URL = 'https://api.telegram.org/bot'
+TOKEN = os.environ.get('SE-CW_TOKEN') # токен бота загружается из переменных среды
+API_URL = 'https://api.telegram.org/bot' # сюда отправляются запросы для telegram bot api
 
-# тут начинаем long-polling
-offset = -2
+# начинаем long-polling
+offset = -2 # чтобы получить все апдейты пока бот не работал
 while True:
     updates = requests.get(f'{API_URL}{TOKEN}/getUpdates?offset={offset + 1}').json()
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            if 'photo' in result['message']:
+    if updates['result']: # проверяем есть ли вообще апдейты
+        for result in updates['result']: # пробегаемся по всем апдейтам в цикле
+            user_id = result['message']['from']['id'] # сохраняем id пользователя (совпадает с id чата)
+            offset = result['update_id'] # чтобы не получать старые апдейты
+            if 'photo' in result['message']: # есть ли в сообщении фото
                 print('Отправлено фото')
-            else:
-                print('Отправлено не фото')
+            else: # если отправлено не фото, то сообщить об этом пользователю
+                response = requests.get(url=f'{API_URL}{TOKEN}/sendMessage',
+                                        params={
+                                            'chat_id': user_id,
+                                            'text': 'Боту можно отправлять только изображения!'
+                                        })
 
     time.sleep(3)
